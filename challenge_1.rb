f6734e9aa39b4e5d58866ac57f4cce87ba79be30
Todd S <starco58@gmail.com>
2014-04-27 09:03:14 -0500

    different formula
    I've moved the variables outside the method, which was my mistake
    yesterday.  I am now playing with different forms of the formula, which
    appear to not be triggering an error; so I assume the formula is right
    and I am not properly inserting the variables

--- challenge_1.rb
+++ challenge_1.rb
@@ -18,8 +18,9 @@ def pmt(rate,nper,pv)
   # Your code to implement the method goes here.
   # ============================================
     # i= (1+rate/12)**(12/12)-1
-    annuity = ((1+rate/12)**(12/12)-1)/((1-(1/(1+rate))**nper)/1)
-      pmt = pv/annuity
+    # annuity = (1-(1/(1+rate))**nper)/1
+    #   pmt = pv/annuity
+    pmt=((rate/100/12)*pv)/(1-((1+(rate/100/12))**(-nper*12)))
 
   return pmt
 end
