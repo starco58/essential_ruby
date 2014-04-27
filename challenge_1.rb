cc13a91f0850456378287d7c7173d0b88a62fdbe
Todd S <starco58@gmail.com>
2014-04-27 08:56:48 -0500

    well, not getting errors.....
    but not getting a result.

--- challenge_1.rb
+++ challenge_1.rb
@@ -17,9 +17,10 @@ def pmt(rate,nper,pv)
   # ============================================
   # Your code to implement the method goes here.
   # ============================================
-    i= (1+rate/12)**(12/12)-1
-    annuity = (1-(1/(1+i))**nper)/1
+    # i= (1+rate/12)**(12/12)-1
+    annuity = ((1+rate/12)**(12/12)-1)/((1-(1/(1+rate))**nper)/1)
       pmt = pv/annuity
+
   return pmt
 end
 
